bc357b67aa13b9b0070e511903db3f7bd72a394e
Sara Rossow <rossowka@icloud.com>
2022-11-16 19:25:53 +0100

    added: button, triggering dialog functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React, { useState } from 'react';
+import { Dialog } from './components/Dialog';
+
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = (e) => {
+    setIsActive(e => !e);
+  };
+
   return (
     <div>
-
+      <button onClick={handleClick}>DELETE</button>
+      {isActive ? <Dialog /> : console.log(false) }
     </div>
   );
 }
---
+++ src/components/Dialog/Dialog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Dialog() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Dialog;
